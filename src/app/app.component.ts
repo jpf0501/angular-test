c80c2e0e55a0d544ef6385513ccf8df9e2567b41
justine <justinefer@airmail.cc>
2023-05-04 11:53:26 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Hello World!</h1>',
+  template: '<h1>Hello World!</h1><app-test></app-test>',
   // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
